cdb10259eb7c946e8c1d0c18c90deb07c2fdba6e
unknown <davmuri1414@gmail.com>
2021-03-13 15:22:41 +0100

    added new images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>Laravel</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
         <link href="{{asset('css/style.css')}}" rel="stylesheet">
-
     </head>
     <body class="antialiased">
        <div id="app">
